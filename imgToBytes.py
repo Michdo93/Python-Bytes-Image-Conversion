afbfc5314494ef201e8f30e4fcc32e6f4745214f
Michdo93 <michaeldoerflinger93@gmail.com>
2022-08-13 11:21:27 +0200

    Update imgToBytes.py

--- imgToBytes.py
+++ imgToBytes.py
@@ -1,1 +1,19 @@
+import base64
+import io
+import cv2
 
+filename = "/home/ubuntu/reconstructed.jpg"
+
+img = cv2.imread(filename)
+height, width, channels = img.shape
+print(height)
+print(width)
+print(channels)
+
+retval, buffer = cv2.imencode('.jpg', img)
+jpg_as_text = base64.b64encode(buffer)
+
+print(jpg_as_text)
+
+with open('/home/ubuntu/testImage2', 'wb') as file_to_save:
+    file_to_save.write(jpg_as_text)
